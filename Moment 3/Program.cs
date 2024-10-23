9cd0afd3d427a93e050362e119df402009bc40de
Leander Norberg <89856214+Crokuz@users.noreply.github.com>
2024-10-23 13:41:32 +0200

    Add files via upload

---
+++ Moment 3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Leander Norberg
+leno2003@student.miun.se
+Programmering i C#.NET (DT071G)
+Moment 3
+*/
+
+namespace Moment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var postManager = new PostManager();
+            // Loads posts from json-file if available
+            postManager.LoadPosts(); 
+
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                // Displays user interface
+                displayMenu = UserInterface.DisplayMenu(postManager);
+            }
+        }
+    }
+}
